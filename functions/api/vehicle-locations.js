f975e1567104b9d1733d85b5ad3608330e00bc8f
Josh McArthur <joshua.mcarthur@gmail.com>
2023-11-17 11:52:11 +1300

    Initial commit

---
+++ functions/api/vehicle-locations.js
@@ -0,0 +1,23 @@
+// Define the URL of the JSON file you want to fetch
+const jsonURL = "https://api.opendata.metlink.org.nz/v1/gtfs-rt/vehiclepositions";
+
+// Set cache expiration time (10 seconds)
+const cacheDuration = 10;
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(context) {
+  // Fetch the JSON content
+  const response = await fetch(jsonURL, { headers: { Authorization: `Bearer ${context.env.METLINK_API_KEY}` } });
+
+  // Create a new response with necessary headers
+  const newResponse = new Response(response.body, response);
+
+  // Set cache headers for the response to cache at Cloudflare's edge locations
+  newResponse.headers.set("Cache-Control", `public, max-age=${cacheDuration}`);
+  newResponse.headers.set("Edge-Control", `public, max-age=${cacheDuration}`);
+
+  return newResponse;
+}
